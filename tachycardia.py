10b662ff20ccaa0a4bb3b5c3ca8e8ab3d45837d2
kevinmchu <kmc100@duke.edu>
2019-02-01 19:17:59 -0500

    testing for exact match

---
+++ tachycardia.py
@@ -0,0 +1,17 @@
+'''
+is_tachycardic.py
+Author: Kevin Chu
+Last Modified: 2/1/19
+
+DESCRIPTION: This program takes a string as an input and determines whether
+it contains the word "tachycardic".
+'''
+
+
+def is_tachycardic(test_str):
+    isTachycardic = False
+
+    if test_str == "tachycardic":
+        isTachycardic = True
+
+    return isTachycardic
